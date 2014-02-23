78a61c4b6a9abfe512caaa80c9e03a3c2f4cca35
Brian Burns <bburns.km@gmail.com>
2014-02-23 06:55:28 -0600

    added comments and flag-text fn

--- filter-lines.el
+++ filter-lines.el
@@ -1,9 +1,36 @@
 
+; filter-lines
+; filter lines interactively
 
+;;; description
+;
+; buffer-local minor mode
+;
+; author: brian burns <brianburns@utexas.edu>
+; url: github...
+; history:
+; 2014-01 started
+;
+; see also
+; keep-lines, flush-lines
+; hide-lines.el
+;
 ; license: gnu. see end of file
 
 
-;;; filter-lines
+;;; notes
+
+; can insert lines in filter mode
+; but m-g will carry invisible lines surrounding it also
+; because of the way it's written - which is why it works with org subtrees also
+
+
+
+;;; private
+
+(defun flag-text (start end) 
+  (overlay-put (make-overlay start end) 'invisible 'filter-lines))
+
 
 (defun filter-lines (&optional regexp)
   "Hide lines that don't match REGEXP. Call with no argument or nil to restore them."
@@ -17,17 +44,18 @@
       (goto-char (point-min))
       (let ((start (point-min))
              ; re-search sets point to end of match, returns point. t means return nil if fail
-            (pos (re-search-forward regexp nil t))
-            (flag-text (lambda (start end) (overlay-put (make-overlay start end) 'invisible 'filter-lines))))
+            (pos (re-search-forward regexp nil t)))
         (while pos
           (beginning-of-line)
-          (funcall flag-lines start (point))
+          ; (funcall flag-lines start (point))
+          (flag-text start (point))
           (forward-line 1) ; move to start of next line
           (setq start (point))
           (if (eq (point) (point-max))
               (setq pos nil)
             (setq pos (re-search-forward regexp nil t))))
-        (funcall flag-text start (point-max))
+        ; (funcall flag-text start (point-max))
+        (flag-text start (point-max))
         (add-to-invisibility-spec 'filter-lines) ; hide flagged lines
         ))))
 
@@ -48,6 +76,7 @@
 ; (defun mark-visible () (remove-overlays (point-min) (point-max) 'invisible 'filter-lines))
 
 
+;;; keymap
 
 (defvar filter-lines/keymap (make-sparse-keymap) "Filter-lines mode keybindings")
 ; (suppress-keymap modern-modal/keymap) ; turn off all printing characters, eg 'p'
@@ -55,14 +84,20 @@
 ; m-q shadowed by modern mode. but m-4 isn't
 ; (define-key filter-lines/keymap "C-M-q" (lambda () (interactive) (filter-lines-mode 0)))
 
+;;; mode
 
 (define-minor-mode filter-lines-mode
   "A minor mode to selectively hide lines matching a regexp."
   ; :lighter " Filter"
-  ; :lighter (:eval (propertize " Filter" 'face 'mode-line-emphasis))
-  :lighter (:eval (propertize " {Filter}" 'face 'mode-line-emphasis))
+  ; :lighter (:eval (propertize " Filter " 'face 'mode-line-emphasis))
+  ; :lighter (:eval (concat " " (propertize " Filter " 'face 'mode-line-emphasis)))
+  ; :lighter (:eval (concat " " (propertize "Filter" 'face 'mode-line-emphasis)#("Filter" 0 6 (face mode-line-emphasis))))
+  ; :lighter (:eval (concat " " "hi"))
+  ; :lighter (:eval (concat " " (concat "hi" "there")))
+  :lighter #(" Filter" 0 3 (face mode-line-emphasis)) ; colors whole word
+  ; :lighter #(" Filter" 1 6 (face mode-line-emphasis))
   :keymap filter-lines/keymap
-  :global t
+  :global nil
   ; body is run every time mode is turned on or off
   (message (if filter-lines-mode "Filter on" "Filter off"))
   (if filter-lines-mode
@@ -74,14 +109,14 @@
       ; (line-numbers-show)
       ;. remember lnm state
       ; (line-number-mode 1)
-      (view-line-numbers 1)
+      ; (view-line-numbers 1)
       (call-interactively 'filter-lines)
       )
     (progn
       ; (set-hl-line-face 'hl-line)
       ; (line-number-mode 0)
-      (view-line-numbers 0)
-      (filter-lines) ; show lines
+      ; (view-line-numbers 0)
+      (filter-lines) ; show hidden lines
       ))
 )
 
@@ -89,21 +124,40 @@
 ; (filter-lines-mode 1)
 ; (filter-lines-mode 0)
 
-(global-set-key (kbd "M-4") 'filter-lines-mode)
+
+;.. m-f m-l
+(global-set-key (kbd "M-4 f") 'filter-lines-mode)
 
 
+;;; afterword
 
 (provide 'filter-lines)
 
 
-;;;; hide-lines package
+;;; hide-lines package
 ; (require 'hide-lines)
 ;. nowork interactively. fix. 
 ; (hide-lines "filter")
 ; (hide-lines "lines")
 ; (hide-lines)
 ; (hide-lines-not-matching "lines")
-;; (defun filter-lines ...)
+; (defun filter-lines ...)
 ;. nowork
 ; (global-set-key (kbd "C-c /") 'hide-lines)
 
+;;; license
+;
+; This program is free software; you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation; either version 2, or (at your option)
+; any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program; see the file COPYING.  If not, write to the
+; Free Software Foundation, Inc., 51 Franklin Street, Fifth
+; Floor, Boston, MA 02110-1301, USA.
